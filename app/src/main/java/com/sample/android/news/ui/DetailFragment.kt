afda33b2809bad36d3d93620be0d4da3b3a8e115
Ali Rezaei <ali.rezaei09@gmail.com>
2019-05-11 02:52:45 +0430

    Add hyperlink to display content

--- app/src/main/java/com/sample/android/news/ui/DetailFragment.kt
+++ app/src/main/java/com/sample/android/news/ui/DetailFragment.kt
@@ -1,6 +1,9 @@
 package com.sample.android.news.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +12,14 @@ import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
 import com.sample.android.news.R
 import com.sample.android.news.databinding.FragmentDetailBinding
+import com.sample.android.news.util.fromHtml
 
 class DetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val args = DetailFragmentArgs.fromBundle(arguments!!)
         val binding = FragmentDetailBinding.inflate(inflater, container, false).apply {
-            article = DetailFragmentArgs.fromBundle(arguments!!).article
+            article = args.article
         }
 
         sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(R.transition.move)
@@ -22,6 +27,11 @@ class DetailFragment : Fragment() {
         with(binding) {
             toolbar.apply {
                 setNavigationOnClickListener { findNavController().navigateUp() }
+            }
+            content.text = TextUtils.expandTemplate(getText(R.string.content), args.article.url).toString().fromHtml()
+            content.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(args.article.url))
+                startActivity(intent)
             }
         }
 
---
+++ app/src/main/java/com/sample/android/news/util/StringExt.kt
@@ -0,0 +1,11 @@
+package com.sample.android.news.util
+
+import android.os.Build
+import android.text.Html
+
+@Suppress("DEPRECATION")
+fun String.fromHtml() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
+} else {
+    Html.fromHtml(this)
+}
